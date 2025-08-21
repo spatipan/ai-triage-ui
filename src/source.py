266d982670fcf63b4ac28099cece01a1ebd99977
Patipan Sitthiprawiat <patipan120897@gmail.com>
2025-08-21 07:06:20 +0700

    fix

--- src/source.py
+++ src/source.py
@@ -215,8 +215,8 @@ class DataPreprocessing:
         self.y_cols = None
 
         self.num_preprocessor = None
-        self.text_embedder_path = 'https://www.kaggle.com/models/google/universal-sentence-encoder/tensorFlow2/multilingual'
-        self.text_embedder = TextEmbedder(self.text_embedder_path)
+        # self.text_embedder_path = 'https://www.kaggle.com/models/google/universal-sentence-encoder/tensorFlow2/multilingual'
+        # self.text_embedder = TextEmbedder(self.text_embedder_path)
 
     def import_data(self, data, x_num_cols, x_text_cols, y_cols):
         train, val, test = data
